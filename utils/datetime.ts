17ed087e281b7eb339ee30f48afffc29a0a76930
dragonwocky <thedragonring.bod@gmail.com>
2023-03-19 15:27:47 +1100

    fix: patch week range gen, use utc + end relative to start

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,14 +1,14 @@
 const formatDate = (date: Date) => {
     const yyyy = date.getUTCFullYear(),
       mm = String(date.getUTCMonth() + 1).padStart(2, "0"),
-      dd = String(date.getDate() + 1).padStart(2, "0");
+      dd = String(date.getUTCDate() + 1).padStart(2, "0");
     return `${yyyy}-${mm}-${dd}`;
   },
   getWeek = (date: Date | string) => {
-    const start = new Date(date),
-      end = new Date(date);
-    start.setDate(start.getDate() - start.getDay() + 1);
-    end.setDate(end.getDate() + 6 - start.getDay());
+    const start = new Date(date);
+    start.setUTCDate(start.getUTCDate() - start.getUTCDay());
+    const end = new Date(start);
+    end.setUTCDate(end.getUTCDate() + 6);
     return `${formatDate(start)}_${formatDate(end)}`;
   };
 
